e26d75c4ea0a847d0b7a1ab40353050fb13205f0
evgeniia lemanova <jane87.05@mail.ru>
2023-04-27 22:12:08 +0300

    add tables

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,25 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+first_name varchar(50) NOT NULL,
+last_name varchar(50) NOT NULL,
+title varchar(50) NOT NULL,
+birth_date date NOT NULL,
+notes text
+);
+
+CREATE TABLE customers
+(
+customer_id char(5) PRIMARY KEY,
+company_name varchar(50) NOT NULL,
+contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+order_id int PRIMARY KEY,
+customer_id char(5) REFERENCES customers(customer_id) NOT NULL,
+employee_id int NOT NULL,
+order_date date NOT NULL,
+ship_city varchar(100) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+# def instantiate_from_csv(cls, data='../src/items.csv'):
+#     '''инициализирует экземпляры класса Item из файла src/items.csv'''
+
+
+with open(data='../north_data/customers_data.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    all = []
+    for row in reader:
+        if row['name'] and row['price'] and row['quantity'] is not None:
+            all.append(row['name'], row['price'], row['quantity'])#connect to db
+
+
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='006711Vf') as conn:
+    with conn.cursor() as cur:
+            # execute query
+        cur.execute('INSERT INTO user_account Values (%s, %s)', (6, 'Jek'))
+
+        cur.execute('SELECT * FROM user_account')
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+            #close cursor and close connection
+
+conn.close()
